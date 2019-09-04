ecf41b3aedb8229319f809e2cbccdee4e1840c8a
tdallas <tdallas@itba.edu.ar>
2019-09-04 19:56:21 -0300

    added basic modeling for rooms

--- models/src/main/java/ar/edu/itba/paw/models/room/Room.java
+++ models/src/main/java/ar/edu/itba/paw/models/room/Room.java
@@ -8,13 +8,12 @@ import lombok.NoArgsConstructor;
 public class Room {
 
     private long id;
-   // private RoomType roomType;
+    private RoomType roomType;
     private boolean free;
     private int number; // > 0
 
-    public Room(/*RoomType roomType*/long id, int number) {
-       // this.roomType = roomType;
-        this.id = id;
+    public Room(RoomType roomType, int number) {
+        this.roomType = roomType;
         this.number = number;
         this.free = false;
     }
