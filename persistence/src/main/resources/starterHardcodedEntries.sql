441a9e3e15594bbc771b92ec20a16cc798b1add5
tdallas <tdallas@itba.edu.ar>
2019-10-07 22:03:22 -0300

    fix hardcoded entries on sql

--- persistence/src/main/resources/starterHardcodedEntries.sql
+++ persistence/src/main/resources/starterHardcodedEntries.sql
@@ -8,10 +8,10 @@ insert into room (id, room_type, is_free_now, number) values (4, 'DOUBLE', true,
 insert into room (id, room_type, is_free_now, number) values (5, 'DOUBLE', true, 106);
 
 -- Hardcoded Entries For Products
-insert into product (id, description, price, filePath, enable) values (1, 'Coca-Cola', 45.3, 'filePath', true);
-insert into product (id, description, price, filePath, enable) values (2, 'Whisky', 150, 'filePath', true);
-insert into product (id, description, price, filePath, enable) values (3, 'Papitas', 12, 'filePath', true);
-insert into product (id, description, price, filePath, enable) values (4, 'Chocolates', 19, 'filePath', true);
+insert into product (id, description, price, file_path, enable) values (1, 'Coca-Cola', 45.3, 'filePath', true);
+insert into product (id, description, price, file_path, enable) values (2, 'Whisky', 150, 'filePath', true);
+insert into product (id, description, price, file_path, enable) values (3, 'Papitas', 12, 'filePath', true);
+insert into product (id, description, price, file_path, enable) values (4, 'Chocolates', 19, 'filePath', true);
 
 -- Hardcoded Entries For Users
 insert into users (id, email, password, username, role)
