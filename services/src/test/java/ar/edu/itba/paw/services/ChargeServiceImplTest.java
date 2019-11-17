6691a12e81003eb3f8d198da74aab9631ce80c0f
tdallas <tdallas@itba.edu.ar>
2019-11-17 19:28:28 -0300

    added all test cases to implement

---
+++ services/src/test/java/ar/edu/itba/paw/services/ChargeServiceImplTest.java
@@ -0,0 +1,34 @@
+package ar.edu.itba.paw.services;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ChargeServiceImplTest {
+
+    @Test
+    public void getAllChargesByValidReservationId() {
+
+    }
+
+    @Test
+    public void getAllChargesByInvalidReservationId() {
+
+    }
+
+    @Test
+    public void getAllChargesNotDelivered() {
+
+    }
+
+    @Test
+    public void setValidChargeToDelivered() {
+
+    }
+
+    @Test
+    public void setInvalidChargeToDelivered() {
+
+    }
+}
---
+++ services/src/test/java/ar/edu/itba/paw/services/UserServiceImplTest.java
@@ -0,0 +1,45 @@
+package ar.edu.itba.paw.services;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceImplTest {
+
+    @Test
+    public void getProductsBoughtByValidReservationIdTest() {
+
+    }
+
+    @Test
+    public void getProductsBoughtByNonValidReservationIdTest() {
+
+    }
+
+    @Test
+    public void addChargeWithValidProductAndValidReservationId() {
+
+    }
+
+    @Test
+    public void addChargeWithInvalidProductAndValidReservationId() {
+
+    }
+
+    @Test
+    public void addChargeWithInvalidProductAndInvalidReservationId() {
+
+    }
+
+    @Test
+    public void getUserForReservationWithAlreadyExistentUser() {
+
+    }
+
+    @Test
+    public void getUserForReservationWithNonExistentUser() {
+
+    }
+
+}
