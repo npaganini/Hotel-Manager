745934785c7984a760bdf4582ad1ca6bc1946156
tdallas <tdallas@itba.edu.ar>
2021-01-06 17:06:06 -0300

    fix: FUCKING FIX

--- webapp/src/main/java/ar/edu/itba/paw/webapp/dto/ReservationDTO.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/dto/ReservationDTO.java
@@ -9,12 +9,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Calendar;
 import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement
 public class ReservationDTO {
     private Long id;
     private Calendar startDate;
