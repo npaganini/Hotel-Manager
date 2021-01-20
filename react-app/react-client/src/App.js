ec70214829ffb252c10396bc29525bb733dd6491
tdallas <tdallas@itba.edu.ar>
2021-01-20 17:58:09 -0300

    feat: fix reservation compile error. Added onClick props on button

--- react-app/react-client/src/App.js
+++ react-app/react-client/src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-import Navbar from './components/Navbar/Navbar'
-import Table from './components/Table/Table'
-import DatePicker from './components/DatePickers/DatePicker'
-import Button from './components/Button/Button'
-import Input from './components/Input/Input'
-import Dropdown from './components/Dropdown/Dropdown'
-import Login from './containers/Login/Login'
-import Principal from './containers/Principal/Principal'
-import Reservation from './containers/Reservation/Reservation'
-import CheckIn from './containers/Check-In/Check-In'
+import Navbar from "./components/Navbar/Navbar";
+import Table from "./components/Table/Table";
+import DatePicker from "./components/DatePickers/DatePicker";
+import Button from "./components/Button/Button";
+import Input from "./components/Input/Input";
+import Dropdown from "./components/Dropdown/Dropdown";
+import Login from "./containers/Login/Login";
+import Principal from "./containers/Principal/Principal";
+import { Reservation } from "./containers/Reservation/Reservation";
+import CheckIn from "./containers/Check-In/Check-In";
 
 class App extends Component {
   render() {
--- react-app/react-client/src/containers/Reservation/Reservation.js
+++ react-app/react-client/src/containers/Reservation/Reservation.js
@@ -1,79 +1,73 @@
-import {React, useState} from 'react';
-import { Container, Row, Col } from 'react-bootstrap'
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { makeStyles } from "@material-ui/core/styles";
 
-import Navbar from '../../components/Navbar/Navbar'
-import Button from '../../components/Button/Button'
-import DatePicker from '../../components/DatePickers/DatePicker'
-import Dropdown from '../../components/Dropdown/Dropdown';
+import Navbar from "../../components/Navbar/Navbar";
+import Button from "../../components/Button/Button";
+import DatePicker from "../../components/DatePickers/DatePicker";
+import Dropdown from "../../components/Dropdown/Dropdown";
 
 const useStyles = makeStyles((theme) => ({
-    container: {
-        background: '#FAF6FC',
-        height: '100vh',
-        maxWidth: '100%',
-        padding: 0
-    },
-    row: {
-        marginTop: '40px',
-        paddingLeft: '10%',
-        paddingRight: '10%'
-    },
-    buttonCol:{
-        textAlign:'center'
-    }
+  container: {
+    background: "#FAF6FC",
+    height: "100vh",
+    maxWidth: "100%",
+    padding: 0,
+  },
+  row: {
+    marginTop: "40px",
+    paddingLeft: "10%",
+    paddingRight: "10%",
+  },
+  buttonCol: {
+    textAlign: "center",
+  },
 }));
 
+export const Reservation = (props) => {
+  const classes = useStyles();
 
+  const [showRooms, show] = useState(false);
 
-const reservation = (props) => {
+  const showRoomsHandler = () => {
+    console.log("cambiando state");
+    show(!showRooms);
+  };
 
-    const classes = useStyles();
+  console.log("showRooms", showRooms);
 
-    const[showRooms, show] = useState(false);
-
-    const showRoomsHandler = () => {
-        if({showRooms})
-            show({showRooms: true});
-    }
-
-    let availableRooms = null;
-
-    // if({showRooms}){
-    //     availableRooms = <Dropdown/>;
-    // }
-
-    
-    return (
-        <div>
-            <Container className={classes.container}>
-                <Row>
-                    <Col>
-                        <Navbar></Navbar>
-                    </Col>
-                </Row>
-                <Row className={classes.row}>
-                    <Col xs={12} md={6}>
-                        <DatePicker Id="from" label="Desde"></DatePicker>
-                    </Col>
-                    <Col xs={12} md={6}>
-                        <DatePicker Id="to" label="Hasta"></DatePicker>
-                    </Col>
-                </Row>
-                <Row className={classes.row}>
-                    <Col xs={12} md={12} className={classes.buttonCol}>
-                        <Button ButtonType="Inherit" Id="search-availability" ButtonText="Buscar Disponibles" onClick={showRoomsHandler}></Button>
-                    </Col>
-                </Row>
-                <Row className={classes.row}>
-                    <Col xs={12} md={4}>
-                        {availableRooms}
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-
-    );
-}
-
-export default reservation;
+  return (
+    <div>
+      <Container className={classes.container}>
+        <Row>
+          <Col>
+            <Navbar></Navbar>
+          </Col>
+        </Row>
+        <Row className={classes.row}>
+          <Col xs={12} md={6}>
+            <DatePicker Id="from" label="Desde"></DatePicker>
+          </Col>
+          <Col xs={12} md={6}>
+            <DatePicker Id="to" label="Hasta"></DatePicker>
+          </Col>
+        </Row>
+        <Row className={classes.row}>
+          <Col xs={12} md={12} className={classes.buttonCol}>
+            <Button
+              ButtonType="Inherit"
+              Id="search-availability"
+              ButtonText="Buscar Disponibles"
+              onClick={showRoomsHandler}
+            ></Button>
+          </Col>
+        </Row>
+        <Row className={classes.row}>
+          <Col xs={12} md={4}>
+            {showRooms && <Dropdown />}
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
