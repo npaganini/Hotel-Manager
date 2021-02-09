e47e06f7a5438991736c62678237776d71919b8f
tdallas <tdallas@itba.edu.ar>
2021-02-09 19:50:44 -0300

    feat: finishing registration integrationQ

--- react-app/react-client/src/api/roomApi.js
+++ react-app/react-client/src/api/roomApi.js
@@ -8,7 +8,7 @@ const postReservationUrl = baseRoomURL + "/reservation";
 const postCheckingUrl = id => baseRoomURL + `/checkin/${id}`;
 const postCheckoutUrl = id => baseRoomURL + `/checkout/${id}`;
 const postSendOrdersTooRoomUrl = id => baseRoomURL + `/orders/${id}`;
-const postRegisterOccupantsUrl = hash => baseRoomURL + `/occupants/${hash}`;
+const postRegisterOccupantsUrl = id => baseRoomURL + `/occupants/${id}`;
 
 export const getAllRooms = async () => get(getAllRomsUrl);
 export const getFreeRooms = async (params) => get(getFreeRoomsUrl, params);
@@ -16,5 +16,5 @@ export const doReservation = async (body) => post(postReservationUrl, body);
 export const doCheckin = async (body, id) => post(postCheckingUrl(id), body);
 export const doCheckout = async (body, id) => post(postCheckoutUrl(id), body);
 export const sendOrderToRoom = async (body, id) => post(postSendOrdersTooRoomUrl(id), body);
-export const registerOccupants = async (body, hash) => post(postRegisterOccupantsUrl(hash), body);
+export const registerOccupants = async (body, id) => post(postRegisterOccupantsUrl(id), body);
 
